b6043d8ad374b71f3fda6d256c72c3230d4b475e
gabriela ortiz <gabriela_ortiz1@alumni.brown.edu>
2023-03-02 14:03:10 -0500

    trying to figure out how make all inputs show up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var APIKey = "1a86d10356127f6c83e94caad377236d";
 var searchBtn = $('#search-btn');
-var cityInput = $('#city-name-input');
-var searchHistoryContainer = $('#search-history')
+var cityInput = $('.city-name-input');
+var searchHistoryContainer = $('.search-history')
 //WHEN BUTTON IS CLICKED TO SEARCH CITY THAT WAS INPUT
 //IT IS SAVED TO LOCAL STORAGE AND APPEARS ON THE LIST BELOW AS A BUTTON THAT CAN BE RECLICKED
 //AND IT WILL FETCH API INFORMATION REGARDING THE WEATHER (CITY NAME, DATE, ICON, TEMPERATURE, HUMIDITY, AND WIND SPEED) OF THAT CITY USING A COMBINATION OF 5 DAY WEATHER FORECAST API AND THE OPENWEATHERMAP API
@@ -10,17 +10,15 @@ var searchHistoryContainer = $('#search-history')
 $(document).ready(function() {
 //click event function
 searchBtn.click(function (event) {
-    var cityName = cityInput.val();
+    event.preventDefault();
 
-    console.log(cityName);
-
-    if (cityName) {
-        //add a button to the search history container
-        var newBtn = searchHistoryContainer.append('<button></button>').addClass('btn btn-secondary').text(cityName);
-
-       cityInput.val("");
-
-       event.preventDefault();
+    var cityNames = cityInput.val();
+    
+        console.log(cityNames)
+    if (cityNames) {
+        //add a button to the search history container with user input
+        var newBtn = searchHistoryContainer.append('<button></button>').addClass('btn btn-secondary').text(cityNames);
+        inputs.val("");
     }
-});
+})
 });
