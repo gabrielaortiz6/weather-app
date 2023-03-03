9499fdf93c5260903895cef1d810e47c584ff07c
gabriela ortiz <gabriela_ortiz1@alumni.brown.edu>
2023-03-03 11:23:43 -0500

    created search button history list with retrieved storage, but it is repeating

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,20 +4,57 @@ var searchHistoryContainer = $('.search-history');
 var userInput = $('.city-name-input');
 //variable that is empty array to store inputs into local storagge
 var cityNamesArray = [];
+
 //WHEN BUTTON IS CLICKED TO SEARCH CITY THAT WAS INPUT
 //IT IS SAVED TO LOCAL STORAGE AND APPEARS ON THE LIST BELOW AS A BUTTON THAT CAN BE RECLICKED
 //AND IT WILL FETCH API INFORMATION REGARDING THE WEATHER (CITY NAME, DATE, ICON, TEMPERATURE, HUMIDITY, AND WIND SPEED) OF THAT CITY USING A COMBINATION OF 5 DAY WEATHER FORECAST API AND THE OPENWEATHERMAP API
 //IF I CLICK ON THE SEARCH HISTORY LIST, THOSE FUNCTIONALITIES REAPPEAR
 
 $(document).ready(function() {
+    function renderCities() {
+        //retrieving the array from loal storage
+       var storedCities = JSON.parse(localStorage.getItem("cities"));
+
+       //if there is something to retrieve in local storage, make it the value of a variable
+       if (storedCities !== null) {
+        cityNamesArray = storedCities;
+       };
+       
+       //for loop in order to index all the values stored in the array
+       for (var i = 0; i < cityNamesArray.length; i++) {
+        var cityName = cityNamesArray[i];
+        var btn = document.createElement("button");
+        var newBtn = $(btn).addClass('btn btn-secondary').text(cityName);
+        searchHistoryContainer.append(newBtn);
+       };
+    };
+    
 //click event function
 searchBtn.click(function (event) {
     event.preventDefault();
+    function renderCities() {
+            //retrieving the array from loal storage
+           var storedCities = JSON.parse(localStorage.getItem("cities"));
+
+           //if there is something to retrieve in local storage, make it the value of a variable
+           if (storedCities !== null) {
+            cityNamesArray = storedCities;
+           };
+
+           console.log(cityNamesArray);
+           
+           //for loop in order to index all the values stored in the array
+           for (var i = 0; i < cityNamesArray.length; i++) {
+            var cityName = cityNamesArray[i];
+            var btn = document.createElement("button");
+            var newBtn = $(btn).addClass('btn btn-secondary').text(cityName);
+            searchHistoryContainer.append(newBtn);
+           }
+        }
 
         var text = userInput.val();
 
         cityNamesArray.push(text);
-        console.log(cityNamesArray);
     
         if (cityNamesText = "") {
             return;
@@ -25,11 +62,6 @@ searchBtn.click(function (event) {
 
         localStorage.setItem("cities", JSON.stringify(cityNamesArray));
         userInput.val("");
-
-    // if (cityNames) {
-    //     //add a button to the search history container with user input
-    //     var newBtn = searchHistoryContainer.append('<button></button>').addClass('btn btn-secondary').text(cityNames);
-    //     inputs.val("");
-    // }
+        renderCities();
 })
 });
