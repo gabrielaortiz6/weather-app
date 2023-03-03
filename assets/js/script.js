483470a66855bceaf356f7443ff2bd8d4c571f9d
gabriela ortiz <gabriela_ortiz1@alumni.brown.edu>
2023-03-03 15:28:28 -0500

    still debugging scope issue of array reiterating instead of updating

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,6 @@ var cityNamesArray = [];
 //AND IT WILL FETCH API INFORMATION REGARDING THE WEATHER (CITY NAME, DATE, ICON, TEMPERATURE, HUMIDITY, AND WIND SPEED) OF THAT CITY USING A COMBINATION OF 5 DAY WEATHER FORECAST API AND THE OPENWEATHERMAP API
 //I WILL NEED TO CONVERT CITY NAME INTO CITY COORDINATES IN ORDER TO USE THE 5-DAY WEATHER FORECAST
 //IF I CLICK ON THE SEARCH HISTORY LIST, THOSE FEATURES REAPPEAR
-
 function retrieveStorage() {
 
     //retrieving the array from local storage
@@ -20,45 +19,44 @@ function retrieveStorage() {
    if (storedCities !== null) {
     cityNamesArray = storedCities;
    };
-   console.log(storedCities);
-}
-
-function renderCities() {
-
-    searchHistoryContainer.innerHTML = "";
-
-   //for loop in order to index all the values stored in the array
-   for (var i = 0; i < cityNamesArray.length; i++) {
-    var cityName = cityNamesArray[i];
-    var btn = document.createElement("button");
-    var newBtn = $(btn).addClass('btn btn-secondary').text(cityName);
-    searchHistoryContainer.append(newBtn);
-   };
 };
 
-
 $(document).ready(function() {
+    retrieveStorage();
+    
+    function renderCities() {
+        
+        searchHistoryContainer.innerHTML = "";
+    
+       //for loop in order to index all the values stored in the array
+       for (var i = 0; i < cityNamesArray.length; i++) {
+        var cityName = cityNamesArray[i];
+        var btn = document.createElement("button");
+        var newBtn = $(btn).addClass('btn btn-secondary').text(cityName);
+        searchHistoryContainer.append(newBtn);
+       };
+    };
 
 //click event function
 searchBtn.click(function (event) {
     event.preventDefault();
 
-        var text = userInput.val();
+    var text = userInput.val();
 
-        //pushes each input into the empty array that will be used to set storage
-        cityNamesArray.push(text);
-    
-        //if there is no input, return
-        if (text = "") {
-            return;
-        }
+    //pushes each input into the empty array that will be used to set storage
+    cityNamesArray.push(text);
 
+    //if there is no input, return
+    if (text = "") {
+        return;
+    }
         //sets local storage with array
         localStorage.setItem("cities", JSON.stringify(cityNamesArray));
         
         //empties the user input area after submitting input with click
         userInput.val("");
 
+        renderCities();
         //code that AskBCS helped with because my code did not update with new input, but instead would display whole array again
         // var city = document.querySelector('.search-history');
         // while(city.firstChild) {
@@ -87,6 +85,4 @@ searchBtn.click(function (event) {
         //function to display inputs stored in local storage as buttons
         //coordinatesAPI();
 })
-retrieveStorage();
-        renderCities();
 });
